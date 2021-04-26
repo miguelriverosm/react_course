00a137ce5c1bab344b4afc0f259c0c44c8a9eee7
Miguel R <miguel.riveros@axity.com>
2021-04-26 04:55:46 -0400

    base

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async( category ) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=Hl2cZJaFZtqn1IuYJEYJXSgKPc5jA3ne&q=${encodeURI(category)}&limit=10`
+  const res = await fetch(url)
+  const { data } = await res.json()
+
+  const gifs = data.map( img => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url
+    }
+  })
+  return gifs
+}
